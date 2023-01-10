e29e179049204357448f1c5dfa8e5f23567273ce
Henry Pick <1henrypick@gmail.com>
2023-01-10 10:34:04 -0800

    light strip project

---
+++ flake.nix
@@ -0,0 +1,10 @@
+{
+	description = "Arduino and avr dev environment";
+
+	outputs = { self, nixpkgs }:
+		let pkgs = nixpkgs.legacyPackages.x86_64-linux;
+	in {
+		devShells.x86_64-linux.default = pkgs.mkShell {packages = [pkgs.arduino-cli];};
+	};
+
+}
